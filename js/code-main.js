33a1ab18847da045a7c292a777116ea654504903
Nox <fp9821@gmail.com>
2018-08-06 14:04:03 +0900

    use highlightjs

---
+++ js/code-main.js
@@ -0,0 +1,8 @@
+hljs.initHighlightingOnLoad();
+
+addEventListener('load', function() {
+  var code = document.querySelector('#code');
+  var worker = new Worker('worker.js');
+  worker.onmessage = function(event) { code.innerHTML = event.data; }
+  worker.postMessage(code.textContent);
+})
