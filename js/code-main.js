4dca6036d971e19a0ec74a5a6f3720c01d94f43e
Nox <fp9821@gmail.com>
2018-08-08 17:09:55 +0900

    fix highlight.js (unoptimized)

--- js/code-main.js
+++ js/code-main.js
@@ -1,32 +1,24 @@
-hljs.initHighlightingOnLoad();
-
-addEventListener('load', function() {
-  var code = document.querySelector('#code');
-  var worker = new Worker('worker.js');
-  worker.onmessage = function(event) { code.innerHTML = event.data; }
-  worker.postMessage(code.textContent);
-})
-
+// TODO: can we make this more efficient?
 $("#tab1").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/pytorch-GV100-bench.txt", function(content) {
-  document.getElementById("tab1").innerHTML = content
+  hljs.highlightBlock(this);
 });
 
 $("#tab2").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/pytorch-P5000-bench.txt", function(content) {
-  document.getElementById("tab2").innerHTML = content
+  hljs.highlightBlock(this);
 });
 
 $("#tab3").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/caffe2-GV100-bench.txt", function(content) {
-  document.getElementById("tab3").innerHTML = content
+  hljs.highlightBlock(this);
 });
 
 $("#tab4").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/caffe2-P5000-bench.txt", function(content) {
-  document.getElementById("tab4").innerHTML = content
+  hljs.highlightBlock(this);
 });
 
 $("#tab5").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/tensorflow-GV100-bench.txt", function(content) {
-  document.getElementById("tab5").innerHTML = content
+  hljs.highlightBlock(this);
 });
 
 $("#tab6").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/tensorflow-P5000-bench.txt", function(content) {
-  document.getElementById("tab6").innerHTML = content
+  hljs.highlightBlock(this);
 });
--- js/worker.js
+++
@@ -1,5 +0,0 @@
-onmessage = function(event) {
-  importScripts('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js');
-  var result = self.hljs.highlightAuto(event.data);
-  postMessage(result.value);
-}
