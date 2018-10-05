5357a9fb472b93a7a3fd8edcca5dd9c3274c1104
Nox <fp9821@gmail.com>
2018-10-05 16:21:46 +0900

    update URL

--- js/code-main.js
+++ js/code-main.js
@@ -1,24 +1,24 @@
 // TODO: can we make this more efficient?
-$("#tab1").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/pytorch-GV100-bench.txt", function(content) {
+$("#tab1").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/framework_wise/pytorch-GV100-bench.txt", function(content) {
   hljs.highlightBlock(this);
 });
 
-$("#tab2").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/pytorch-P5000-bench.txt", function(content) {
+$("#tab2").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/framework_wise/pytorch-P5000-bench.txt", function(content) {
   hljs.highlightBlock(this);
 });
 
-$("#tab3").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/caffe2-GV100-bench.txt", function(content) {
+$("#tab3").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/framework_wise/caffe2-GV100-bench.txt", function(content) {
   hljs.highlightBlock(this);
 });
 
-$("#tab4").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/caffe2-P5000-bench.txt", function(content) {
+$("#tab4").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/framework_wise/caffe2-P5000-bench.txt", function(content) {
   hljs.highlightBlock(this);
 });
 
-$("#tab5").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/tensorflow-GV100-bench.txt", function(content) {
+$("#tab5").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/framework_wise/tensorflow-GV100-bench.txt", function(content) {
   hljs.highlightBlock(this);
 });
 
-$("#tab6").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/tensorflow-P5000-bench.txt", function(content) {
+$("#tab6").load("https://raw.githubusercontent.com/noxouille/deep-learning-benchmark/master/results/framework_wise/tensorflow-P5000-bench.txt", function(content) {
   hljs.highlightBlock(this);
 });
